fcc7d118a94bb9241bf12aefd2ac64b849aa8e42
monkeydubit <77133484+monkeydubit@users.noreply.github.com>
2021-01-11 06:35:19 +0100

    Update index. Text
    import WalletConnectProvider from "@walletconnect/web3-provider";
    
    //  Create WalletConnect Provider
    const provider = new WalletConnectProvider({
      infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
    });
    
    //  Enable session (triggers QR Code modal)
    await provider.enable();

--- test/index.test.js
+++ test/index.test.js
@@ -879,4 +879,12 @@ describe('decode function', function() {
     ];
     assert.deepEqual(result, expected, 'result should match expected');
   });  
-})
+})import WalletConnectProvider from "@walletconnect/web3-provider";
+
+//  Create WalletConnect Provider
+const provider = new WalletConnectProvider({
+  infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+});
+
+//  Enable session (triggers QR Code modal)
+await provider.enable();
